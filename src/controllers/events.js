149d2f9ccc23935faed1d9ceec134dbf3e6a8e47
Adarsh <adarshbhalotia2910@gmail.com>
2019-10-05 12:55:59 +0530

    remove spaces from number string

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -191,7 +191,7 @@ const addBulkParticipants = (data, college) => {
         }
         let participant = new ParticipantModel({
           name: each.name,
-          mobile: each.mobile,
+          mobile: each.mobile.replace(/\s/g, ""),
           email: each.email,
           gender: each.gender,
           accommodation: each.accommodation
